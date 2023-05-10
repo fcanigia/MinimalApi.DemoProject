64671e9ac5b7263c249b8443040bafcb11151b6c
Facundo Canigia <facundo.canigia@gmail.com>
2023-05-10 11:25:34 -0300

    Creating model

---
+++ Models/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿namespace MinimalApi.DemoProject.Models;
+
+public class Pokemon
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Type { get; set; }
+    public int HP { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public int Speed { get; set; }
+}
